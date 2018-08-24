ed6917006d7a3b89f3d3241e03f3db05f4299296
OLETECH\chanchuan.yeh <chanchuan.yeh@gsit.com.tw>
2018-08-24 12:17:20 +0800

    install vscode extension : Vue.js Extension Pack ,for formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,26 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    
-    
-    "parserOptions": {
+  env: {
+    browser: true,
+    es6: true
+  },
+  plugins: ["vue", "html"],
+  extends: ["eslint:recommended", "plugin:vue/essential", "prettier"],
 
-        "ecmaVersion": 2015,
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
-
+  parserOptions: {
+    ecmaVersion: 2015,
+    sourceType: "module"
+  },
+  rules: {
+    indent: ["error", "tab"],
+    "linebreak-style": ["error", "windows"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "no-const-assign": "warn",
+    "no-this-before-super": "warn",
+    "no-undef": "warn",
+    "no-unreachable": "warn",
+    "no-unused-vars": "warn",
+    "constructor-super": "warn",
+    "valid-typeof": "warn"
+  }
 };
