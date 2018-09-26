362897d03d172802b705b0f6efdc0431891fd9b8
OLETECH\chanchuan.yeh <chanchuan.yeh@gsit.com.tw>
2018-09-26 11:48:37 +0800

    123

--- src/components/Withdraw.vue
+++ src/components/Withdraw.vue
@@ -13,7 +13,7 @@
                 </el-select>
             </el-col>
             <el-col :span="8">
-                <span>提現單號：stash1</span>
+                <span>提現單號：123</span>
                 <el-input
                     placeholder="請輸入單號"
                     v-model="inputNO">
